8d747d74da19bb648ea27ff30cd4919df4d46e31
Erik <hello@ernord.se>
2020-12-09 11:07:56 +0100

    moved to package

--- src/tree/MyNode.java
+++ src/tree/MyNode.java
@@ -14,7 +14,7 @@ public class MyNode {
 
   /**
    * Class constructor initializing this node's value.
-   * 
+   *
    * @param value The value of this node.
    */
   public MyNode(int value) {
@@ -109,4 +109,4 @@ public class MyNode {
     }
     return cnt;
   }
-} 
+}
